139b829e6203555be017a71761175e35c03ebe46
pramodhn <pnatara7@asu.edu>
2015-03-29 00:58:34 -0700

    Add pause button

--- js/Viz.js
+++ js/Viz.js
@@ -3,7 +3,10 @@
   d3.csv("http://pramodhn.github.io/top3-updated.csv", function(error, data) {
     data.forEach(function(d) {
   });
-  var selectedYear = 0, selectedIndex = 0,selectedtennisball,position;
+
+  var margin = {top:20,right:20,bottom:20,left:25};
+
+  var selectedYear = 0, selectedIndex = 0, selectedtennisball, position, isPause = false;
 
   // Tool tip function
   var tip = d3.tip()
@@ -15,9 +18,8 @@
 
   // Create SVG element
   var svg = d3.select("body").append("svg").attr("align","center").attr("id","svgMain").attr("height", "506px").attr("width", "1000px").attr("style","background-image: url('img/Court.png')").call(tip);
-
   // Declare variables to be used in the program
-  var net1,net2,first1,first2,return1,return2,netimage1,netimage2,returnimage1,returnimage2,firstimage1,firstimage2,playertext1,playertext2,roundnum,playername1,playername2,playername="",winlossimage;
+  var net1,net2,first1,first2,return1,return2,netimage1,netimage2,returnimage1,returnimage2,firstimage1,firstimage2,playertext1,playertext2,roundnum,playername1,playername2,playername="",winlossimage,pauseButton;
   
   // Create a structure that will be used to store data based on player and year  
   var responsestruct = [];
@@ -28,6 +30,7 @@
   }
 
   // Create all the elements to be used in the visualization
+  pauseButton = document.getElementById('pauseButton').hidden = true;
   winlossimage = svg.append("rect").attr("width",90).attr("height",120).attr("x",455).attr("y",30).style("fill","url(#win)").style("visibility","hidden");
   playername1 = svg.append("text").attr("x",250).attr("y",70).attr("text-anchor","middle").style("font-size","30px");
   playername2 = svg.append("text").attr("x",750).attr("y",70).attr("text-anchor","middle").style("font-size","30px");
@@ -66,11 +69,22 @@
     visualize();
   }
 
+  // Event listener for start stop
+  d3.select('button').on('click', function() {
+    if(d3.select('button').html() == "Resume") {
+      d3.select('button').html("Pause");
+      isPause = false;
+      showTransition(position+1);
+    } else if(d3.select('button').html() == "Pause") {
+      d3.select('button').html("Resume");
+      isPause = true;
+    }
+  });
   // This function sets up the values for 'responsestruct' structure
   function visualize() {
     
     netimage1.style("visibility","hidden");netimage2.style("visibility","hidden");firstimage1.style("visibility","hidden");firstimage2.style("visibility","hidden");returnimage1.style("visibility","hidden");returnimage2.style("visibility","hidden");playername1.style("visibility","hidden");playername2.style("visibility","hidden");roundnum.style("visibility","hidden");winlossimage.style("visibility","hidden");
-    
+    pauseButton = document.getElementById('pauseButton').hidden = true;
     if(selectedIndex == 0 && selectedYear == 0) { // When neither player nor year are chosen
       d3.select("#selection-comment").html("Select a player and year : ");
     } else if(selectedIndex == 0) { // If player is not chosen
@@ -136,8 +150,10 @@
           }
         }
       }
-      if(i>0)
+      if(i>0) {
+        drawHistogram();
         showTransition(0); // Perform the transition
+      }
       if(responsestruct[0].round == "") // If the player has not played any game in that year
         winlossimage.style("visibility","visible").style("fill","url(#dnp)");
     }
@@ -151,7 +167,7 @@
     svg.append("text").attr("x",30).attr("y",253).attr("text-anchor","left").style("font-size","15px").text("Net Approach");
     svg.append("text").attr("x",30).attr("y",323).attr("text-anchor","left").style("font-size","15px").text("Return point");
 
-    if(responsestruct[i].round != "") {
+    if(responsestruct[i].round != ""  && isPause == false) {
       position = i;
       if(responsestruct[i].result == "win"){
           // Transitions in case of victorious game
@@ -165,6 +181,7 @@
           playername1.style("visibility","visible").text(responsestruct[i].p1);
           playername2.style("visibility","visible").text(responsestruct[i].p2);
           winlossimage.transition().delay(1000).style("visibility","visible").style("fill","url(#win)");
+          pauseButton = document.getElementById('pauseButton').hidden = false;
       } else if(responsestruct[i].result == "loss") {
           // Transitions in case of lost game
           netimage1.style("visibility","visible").transition().attr("cx", Number(responsestruct[i].n2)*5).duration(1500);
@@ -177,14 +194,16 @@
           playername1.style("visibility","visible").text(responsestruct[i].p2);
           playername2.style("visibility","visible").text(responsestruct[i].p1);
           winlossimage.transition().delay(1000).style("visibility","visible").style("fill","url(#loss)");
+          pauseButton = document.getElementById('pauseButton').hidden = true;
       }
       winlossimage.style("visibility","hidden");
     }
 
-    if(i<6 && responsestruct[i].round != "") {
+    if(i<6 && responsestruct[i].round != "" && isPause == false) {
         // Disable the dropdowns while animations are going on
         document.getElementById("choose-player").disabled = true;
         document.getElementById("choose-year").disabled = true;
+        pauseButton = document.getElementById('pauseButton').hidden = false;
         setTimeout(function(d){
                 i++;
                 showTransition(i); // Recursive call to the function every 2 seconds
@@ -193,9 +212,59 @@
         // Enable the dropdowns after animations are completed
         document.getElementById("choose-player").disabled = false;
         document.getElementById("choose-year").disabled = false;
+        if(isPause == true)
+          pauseButton = document.getElementById('pauseButton').hidden = false;
+        else
+          pauseButton = document.getElementById('pauseButton').hidden = true;
     }
   }
 
+
+  function drawHistogram() {
+    /*
+    // Create histogram and draw its axes
+    var h1 = d3.select("body").append("svg").attr("id","svgHist1").attr("height", "300px").attr("width", "500px").attr("role","histogram");
+    var hsvg = d3.select('[role="histogram"]');
+    var histogram = hsvg.attr('width',500).attr('height',300).append('g');
+
+    var x0 = d3.scale.ordinal().rangeRoundBands([0, 500], .1);
+
+    var x1 = d3.scale.ordinal();
+    var color = d3.scale.ordinal().range(["#98abc5", "#a6898a", "#887b68"]);
+    var histXAxisScale = d3.scale.linear().range([0,330]).domain([4,14]),
+      histXAxis = d3.svg.axis().orient('bottom').ticks(11).scale(x0),
+      histYAxisScale = d3.scale.linear().range([0,100]).domain([100,0]),
+      histYAxis = d3.svg.axis().orient('left').ticks(5).scale(histYAxisScale);
+    
+    hsvg.append('g').attr('transform','translate('+margin.left*2+','+(margin.top+100)+')').attr('class','x axis').call(histXAxis);
+    hsvg.append('g').attr('transform','translate('+margin.left*2+','+(margin.top)+')').attr('class','y axis').call(histYAxis);
+
+    histogram.selectAll('rect')
+      .data(responsestruct)
+      .enter()
+      .append('rect')
+      .attr('fill','red')
+      .attr('height',function(d) { return Number(d.f1);})
+      .attr('width',20)
+      .attr('x',function(d, i) {
+        return (margin.left*2) + (i*20);
+      });
+      .append('rect')
+      .attr('fill','green')
+      .attr('height',function(d) { return Number(d.n1);})
+      .attr('width',20)
+      .attr('x',function(d, i) {
+        return (margin.left*2) + (i*40);
+      })
+      .append('rect')
+      .attr('fill','blue')
+      .attr('height',function(d) { return Number(d.r1);})
+      .attr('width',20)
+      .attr('x',function(d, i) {
+        return (margin.left*2) + (i*60);
+      });            
+    */
+  }
   // This function returns the name of the round based on the input round number provided
   function roundname(roundnum) {
     if(roundnum==1)
